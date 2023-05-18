3c52fb1fc885926e1311f76e999acec78970f804
Guido Tello <lpmaleinn@gmail.com>
2023-05-18 04:19:16 -0300

    Add GLFW

--- FlyEngine/main.cpp
+++ FlyEngine/main.cpp
@@ -1,14 +1,14 @@
-#include <GLFW/glfw3.h>
+#include <glfw3.h>
 
 int main(void)
 {
     GLFWwindow* window;
 
-    /* Initialize the library */
+    // Initialize the library 
     if (!glfwInit())
         return -1;
 
-    /* Create a windowed mode window and its OpenGL context */
+    // Create a windowed mode window and its OpenGL context 
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
     if (!window)
     {
@@ -16,19 +16,19 @@ int main(void)
         return -1;
     }
 
-    /* Make the window's context current */
+    // Make the window's context current 
     glfwMakeContextCurrent(window);
 
-    /* Loop until the user closes the window */
+    // Loop until the user closes the window 
     while (!glfwWindowShouldClose(window))
     {
-        /* Render here */
+        // Render here 
         glClear(GL_COLOR_BUFFER_BIT);
 
-        /* Swap front and back buffers */
+        // Swap front and back buffers 
         glfwSwapBuffers(window);
 
-        /* Poll for and process events */
+        //Poll for and process events 
         glfwPollEvents();
     }
 
