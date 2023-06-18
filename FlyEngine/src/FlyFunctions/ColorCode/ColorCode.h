ccff3bf58233ebcb3104dd538d8bfd412090f1aa
Guido Tello <lpmaleinn@gmail.com>
2023-06-17 22:54:18 -0300

    Change COLOR Enum to Class Enum

--- FlyEngine/src/FlyFunctions/ColorCode/ColorCode.h
+++ FlyEngine/src/FlyFunctions/ColorCode/ColorCode.h
@@ -8,7 +8,7 @@ namespace FLY_ENGINE
 	
 //#define COLOR_RED 1.0f, 0.0f, 0.0f, 1.0f
 //#define COLOR_FRANCE_BLUE 0.1f, 0.4f, 0.9f, 1.0f
-	enum FLY_API COLOR
+	enum class FLY_API COLOR
 	{
 		WHITE,
 		RAYWHITE,
