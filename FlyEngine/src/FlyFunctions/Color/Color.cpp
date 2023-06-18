00dcb706e669ab89830c1262ad3d872c8edcd6a2
Guido Tello <lpmaleinn@gmail.com>
2023-06-17 22:54:30 -0300

    Update Color Class

--- FlyEngine/src/FlyFunctions/Color/Color.cpp
+++ FlyEngine/src/FlyFunctions/Color/Color.cpp
@@ -1,6 +1,8 @@
 #include "Color.h"
 #include <FlyFunctions/Commons/Commons.h>
 
+using namespace glm;
+
 FLY_ENGINE::Color::Color()
 {
 	red   = 1.0f;
@@ -9,16 +11,43 @@ FLY_ENGINE::Color::Color()
 	alpha = 1.0f;
 }
 
-FLY_ENGINE::Color::~Color()
+FLY_ENGINE::Color::Color(float r, float g, float b, float a)
 {
+	SetColor(r, g, b, a);
 }
 
-void FLY_ENGINE::Color::SetColor(float r, float g, float b, float a)
+FLY_ENGINE::Color::Color(float r, float g, float b)
 {
-	red = r;
-	green = g;
-	blue = b;
-	alpha = a;
+	SetColor(r, g, b);
+}
+
+FLY_ENGINE::Color::Color(int r, int g, int b, int a)
+{
+	SetColor(r, g, b, a);
+}
+
+FLY_ENGINE::Color::Color(int r, int g, int b)
+{
+	SetColor(r, g, b);
+}
+
+FLY_ENGINE::Color::Color(glm::vec4 rgba)
+{
+	SetColor(rgba);
+}
+
+FLY_ENGINE::Color::Color(glm::vec3 rgb)
+{
+	SetColor(rgb);
+}
+
+FLY_ENGINE::Color::Color(COLOR color)
+{
+	SetColor(color);
+}
+
+FLY_ENGINE::Color::~Color()
+{
 }
 
 void FLY_ENGINE::Color::SetColor(float r, float g, float b)
@@ -29,23 +58,36 @@ void FLY_ENGINE::Color::SetColor(float r, float g, float b)
 	alpha = 1.0f;
 }
 
-void FLY_ENGINE::Color::SetColor(int r, int g, int b, int a)
+void FLY_ENGINE::Color::SetColor(float r, float g, float b, float a)
 {
-	red = static_cast<float>(r) / 256;
-	green = static_cast<float>(g) / 256;
-	blue = static_cast<float>(b) / 256;
-	alpha = static_cast<float>(a) / 256;
+	SetColor(r, g, b);
+	alpha = a;
 }
 
 void FLY_ENGINE::Color::SetColor(int r, int g, int b)
 {
-	red = static_cast<float>(r) / 256;
-	green = static_cast<float>(g) / 256;
-	blue = static_cast<float>(b) / 256;
+	if (red < 0) red = 0;
+	if (red > MAX_COLOR_SCALER) red = MAX_COLOR_SCALER;
+	red = static_cast<float>(r) / MAX_COLOR_SCALER;
+	if (green < 0) green = 0;
+	if (green > MAX_COLOR_SCALER) green = MAX_COLOR_SCALER;
+	green = static_cast<float>(g) / MAX_COLOR_SCALER;
+	if (blue < 0) blue = 0;
+	if (blue > MAX_COLOR_SCALER) blue = MAX_COLOR_SCALER;
+	blue = static_cast<float>(b) / MAX_COLOR_SCALER;
 	alpha = 1.0f;
 }
 
-void FLY_ENGINE::Color::SetColor(glm::vec4 rgba)
+void FLY_ENGINE::Color::SetColor(int r, int g, int b, int a)
+{
+	SetColor(r, g, b);
+	if (alpha < 0) alpha = 0;
+	if (alpha > MAX_COLOR_SCALER) alpha = MAX_COLOR_SCALER;
+	alpha = static_cast<float>(a) / MAX_COLOR_SCALER;
+}
+
+
+void FLY_ENGINE::Color::SetColor(vec4 rgba)
 {
 	red = rgba.r;
 	green = rgba.g;
@@ -61,59 +103,72 @@ void FLY_ENGINE::Color::SetColor(glm::vec3 rgb)
 	alpha = 1.0f;
 }
 
+void FLY_ENGINE::Color::SetColor(Color color)
+{
+	red = color.red;
+	green = color.green;
+	blue = color.blue;
+	alpha = color.alpha;
+}
+
+void FLY_ENGINE::Color::SetColor(COLOR color)
+{
+	SetColor(*GetColor(color));
+}
+
 glm::vec4 FLY_ENGINE::Color::GetColor()
 {
 	return glm::vec4(red, green, blue, alpha);
 }
 
-glm::vec4 FLY_ENGINE::Color::GetColor(COLOR color)
+glm::vec4 FLY_ENGINE::Color::GetColorV4(COLOR color)
 {
 	glm::vec4 newColor;
 	newColor.a = 1.0f;
 
 	switch (color)
 	{
-	case FLY_ENGINE::WHITE:
+	case FLY_ENGINE::COLOR::WHITE:
 		newColor.r = 1.0f;
 		newColor.g = 1.0f;
 		newColor.b = 1.0f;
 		break;
-	case FLY_ENGINE::RAYWHITE:
-		newColor.r = 0.95f;
-		newColor.g = 0.95f;
-		newColor.b = 0.95f;
+	case FLY_ENGINE::COLOR::RAYWHITE:
+		newColor.r = 0.92f;
+		newColor.g = 0.92f;
+		newColor.b = 0.92f;
 		break;
-	case FLY_ENGINE::RED:
+	case FLY_ENGINE::COLOR::RED:
 		newColor.r = 1.0f;
 		newColor.g = 0.0f;
 		newColor.b = 0.0f;
 		break;
-	case FLY_ENGINE::GREEN:
+	case FLY_ENGINE::COLOR::GREEN:
 		newColor.r = 1.0f;
 		newColor.g = 1.0f;
 		newColor.b = 1.0f;
 		break;
-	case FLY_ENGINE::BLUE:
+	case FLY_ENGINE::COLOR::BLUE:
 		newColor.r = 0.0f;
 		newColor.g = 0.0f;
 		newColor.b = 1.0f;
 		break;
-	case FLY_ENGINE::MAGENTA:
+	case FLY_ENGINE::COLOR::MAGENTA:
 		newColor.r = 1.0f;
 		newColor.g = 0.0f;
 		newColor.b = 1.0f;
 		break;
-	case FLY_ENGINE::YELLOW:
+	case FLY_ENGINE::COLOR::YELLOW:
 		newColor.r = 1.0f;
 		newColor.g = 1.0f;
 		newColor.b = 0.0f;
 		break;
-	case FLY_ENGINE::CYAN:
+	case FLY_ENGINE::COLOR::CYAN:
 		newColor.r = 0.0f;
 		newColor.g = 1.0f;
 		newColor.b = 1.0f;
 		break;
-	case FLY_ENGINE::BLACK:
+	case FLY_ENGINE::COLOR::BLACK:
 		newColor.r = 0.0f;
 		newColor.g = 0.0f;
 		newColor.b = 0.0f;
@@ -127,6 +182,11 @@ glm::vec4 FLY_ENGINE::Color::GetColor(COLOR color)
 
 	return newColor;
 
+}
+
+FLY_ENGINE::Color* FLY_ENGINE::Color::GetColor(COLOR color)
+{
+	return new Color(GetColorV4(color));
 }
 
 glm::vec4 FLY_ENGINE::Color::GetRandomColor()
--- FlyEngine/src/FlyFunctions/Color/Color.h
+++ FlyEngine/src/FlyFunctions/Color/Color.h
@@ -5,6 +5,8 @@
 #include <glm/vec4.hpp>
 #include <glm/vec3.hpp>
 
+const int MAX_COLOR_SCALER = 256;
+
 namespace FLY_ENGINE
 {
 	class FLY_API Color
@@ -16,6 +18,13 @@ namespace FLY_ENGINE
 		float alpha;
 	public:
 		Color();
+		Color(float r, float g, float b, float a);
+		Color(float r, float g, float b);
+		Color(int r, int g, int b, int a);
+		Color(int r, int g, int b);
+		Color(glm::vec4 rgba);
+		Color(glm::vec3 rgb);
+		Color(COLOR color);
 		~Color();
 
 		void SetColor(float r, float g, float b, float a);
@@ -24,9 +33,13 @@ namespace FLY_ENGINE
 		void SetColor(int r, int g, int b);
 		void SetColor(glm::vec4 rgba);
 		void SetColor(glm::vec3 rgb);
+		void SetColor(Color color);
+		void SetColor(COLOR color);
+		
 		glm::vec4 GetColor();
 
-		static glm::vec4 GetColor(COLOR color);
+		static glm::vec4 GetColorV4(COLOR color);
+		static Color* GetColor(COLOR color);
 		static glm::vec4 GetRandomColor();
 		
 	};
