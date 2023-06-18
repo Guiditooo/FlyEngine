3353598a2ab464626d08ee0fb459405a19ca1e0a
Guido Tello <lpmaleinn@gmail.com>
2023-06-17 23:00:08 -0300

    Update Renderer Class
    +Added a list that will be the responsible of rendering things based on the entities state (isVisible)
             - Added a function to add entities to that list
             - Added a function that compares the entities draw layer just to sort its draw order.
    +Added a function that will allow the user change the background color.

--- FlyEngine/src/Renderer/Renderer.cpp
+++ FlyEngine/src/Renderer/Renderer.cpp
@@ -1,9 +1,50 @@
 #include "Renderer.h"
+#include <Entity/Entity.h>
+#include <GLFW/glfw3.h>
 
-Renderer::Renderer()
+#include <vector>
+#include <iostream>
+
+std::list<Entity*> Renderer::renderizableObjectList;
+Color* Renderer::bgColor;
+
+void Renderer::DrawRenderizableObjects()
 {
+	for (auto renderizableObject : renderizableObjectList)		
+	{
+		renderizableObject->Draw();
+	}
 }
 
-Renderer::~Renderer()
+#pragma region ORDER RENDERIZABLE OBJECTS
+
+bool CompareByDrawLayer(Entity* a, Entity* b)
 {
+	return a->GetDrawLayer() < b->GetDrawLayer();
+}
+
+void Renderer::ReOrderRenderizableList()
+{
+	renderizableObjectList.sort(CompareByDrawLayer);
+}
+
+#pragma endregion
+
+void Renderer::AddToRenderizableList(Entity* newRenderizableObject)
+{
+	renderizableObjectList.push_back(newRenderizableObject);
+}
+
+
+void Renderer::SetBackgroundColor(Color* newBgColor)
+{
+	bgColor = newBgColor;
+	glClearColor(bgColor->GetColor().r, bgColor->GetColor().g, bgColor->GetColor().b, bgColor->GetColor().a);
+
+	glClear(GL_COLOR_BUFFER_BIT);
+}
+
+Color* Renderer::GetBackgroundColor()
+{
+	return bgColor;
 }
--- FlyEngine/src/Renderer/Renderer.h
+++ FlyEngine/src/Renderer/Renderer.h
@@ -1,13 +1,28 @@
 #ifndef RENDERER_H
 #define RENDERER_H
 
+#include <list>
+#include <FlyFunctions/Color/Color.h>
+
+using namespace FLY_ENGINE;
+
+class Entity;
+
 class Renderer
 {
 private:
+	static Color* bgColor;
 
+	static std::list<Entity*> renderizableObjectList;
+	static void ReOrderRenderizableList();
 public:
-	Renderer();
-	~Renderer();
+	static void DrawRenderizableObjects();
+	static void AddToRenderizableList(Entity* newRenderizableObject);
+	
+	
+	static void SetBackgroundColor(Color* newBgColor);
+	static Color* GetBackgroundColor();
+
 };
 
 #endif // !
