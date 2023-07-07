1623a1b9358240342b375b7331c6b844e23cd59d
Guido Tello <lpmaleinn@gmail.com>
2023-07-07 00:27:06 -0300

    Create VertexAttributes Struct

---
+++ FlyEngine/src/Attribute/VertexAttribute.h
@@ -0,0 +1,17 @@
+#ifndef VERTEX_ATTRIBUTE_H
+#define VERTEX_ATTRIBUTE_H
+
+#include <GLEW/glew.h>
+#include <GLFW/glfw3.h>
+
+struct VertexAttribute
+{
+	unsigned int elementSize = 0;
+	unsigned int variableType = 0;
+	unsigned int isNormalized = 0;
+	unsigned int sizeOfVertex = 0;
+	unsigned int offset = 0;
+};
+
+
+#endif // !ATTRIBUTE_H
