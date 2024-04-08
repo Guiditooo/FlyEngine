fdc0874d4c61dd8b28766faeabdd3bc3b122634f
Guido Tello <lpmaleinn@gmail.com>
2024-04-08 15:33:25 -0300

    Remove Input Up & Pressed(maintain) methods
    Both of them does have a bug that ill not fix at this moment

--- FlyEngine/src/Input/Input.cpp
+++ FlyEngine/src/Input/Input.cpp
@@ -9,41 +9,66 @@ namespace FlyEngine
 
 		Window* Input::window = nullptr;
 
+		//std::unordered_set<KeyCode> Input::pressedKeys;
+
 		void Input::SetContextWindow(Window* newWindow)
 		{
 			window = newWindow;
 		}
-
+		/*
 		bool Input::GetKeyUp(KeyCode key)
 		{
+			if (pressedKeys.empty() || pressedKeys.count(key) != 1) return false;
+
 			if (glfwGetKey(window->GetWindow(), static_cast<int>(key)) == GLFW_RELEASE)
 			{
-				Debugger::ConsoleMessage("Tecla soltada!", 2, 0, 1);
+				Debugger::ConsoleMessageID("Tecla soltada!", 2, 0, 1);
+				pressedKeys.erase(key);
 				return true;
 			}
 			return false;
 		}
-
 		bool Input::GetKeyPressed(KeyCode key)
 		{
 			if (glfwGetKey(window->GetWindow(), static_cast<int>(key)) == GLFW_REPEAT)
 			{
-				Debugger::ConsoleMessage("Tecla Presionada!", 2, 0, 1);
+				Debugger::ConsoleMessageID("Tecla Presionada!", 2, 0, 1);
+				pressedKeys.insert(key);
 				return true;
 			}
 			return false;
 		}
-
+		*/
 		bool Input::GetKeyDown(KeyCode key)
 		{
 			if (glfwGetKey(window->GetWindow(), static_cast<int>(key)) == GLFW_PRESS)
 			{
-				Debugger::ConsoleMessage("Tecla Mantenida Presionada!", 2, 0, 1);
+				Debugger::ConsoleMessageID("Tecla Mantenida!", 2, 0, 1);
+				//pressedKeys.insert(key);
 				return true;
 			}
 			return false;
 		}
-
+		/*
+		void Input::GlfwTester(KeyCode key)
+		{
+			switch (glfwGetKey(window->GetWindow(), static_cast<int>(key)))
+			{
+			case GLFW_PRESS:
+				Debugger::ConsoleMessage("PRESS!", 2, 0, 1);
+				break;
+			case GLFW_REPEAT:
+				Debugger::ConsoleMessage("REPEAT!", 2, 0, 1);
+				break;
+			case GLFW_RELEASE:
+				Debugger::ConsoleMessage("RELEASE!", 2, 0, 1);
+				break;
+			default:
+				Debugger::ConsoleMessage("UNKNOWN!", 2, 0, 1);
+				break;
+			}
+		}
+		*/
 	}
 
 }
--- FlyEngine/src/Input/Input.h
+++ FlyEngine/src/Input/Input.h
@@ -3,7 +3,7 @@
 
 #include <Window/Window.h>
 #include <FlyFunctions/KeyCode/KeyCode.h>
-
+//#include <unordered_set>
 
 namespace FlyEngine
 {
@@ -11,12 +11,14 @@ namespace FlyEngine
 	namespace Utils
 	{
 
-		class FLY_API Input //SI PONGO LA API, TIRA ERROR PREGUNTAR
+		class FLY_API Input
 		{
 		private:
 			static Window* window;
+			//static std::unordered_set<KeyCode> pressedKeys;
 		public:
 			static void SetContextWindow(Window* newWindow);
+			static void GlfwTester(KeyCode key);
 
 			static bool GetKeyUp(KeyCode key);
 			static bool GetKeyPressed(KeyCode key);
