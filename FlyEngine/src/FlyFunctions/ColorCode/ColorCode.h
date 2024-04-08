4734f5dcb8e9e45a7655780969ac99c3894d43dd
Guido Tello <lpmaleinn@gmail.com>
2024-04-08 15:31:18 -0300

    Add color count to ColorCode enum

--- FlyEngine/src/FlyFunctions/ColorCode/ColorCode.h
+++ FlyEngine/src/FlyFunctions/ColorCode/ColorCode.h
@@ -11,6 +11,9 @@ namespace FlyEngine
 
 		//#define COLOR_RED 1.0f, 0.0f, 0.0f, 1.0f
 		//#define COLOR_FRANCE_BLUE 0.1f, 0.4f, 0.9f, 1.0f
+
+		const int COLOR_COUNT = 10;
+
 		enum class FLY_API COLOR
 		{
 			WHITE,
