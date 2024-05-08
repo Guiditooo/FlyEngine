d0ec0cebd38e270bd464a6b466d722242ceb8c98
Guido Tello <lpmaleinn@gmail.com>
2024-05-08 10:15:44 -0300

    Create a Buffer struct
    Just for ordering the structure. Everything that is in need of using buffers should use/have a Buffer member.

---
+++ FlyEngine/src/Buffers/Buffers.h
@@ -0,0 +1,17 @@
+#ifndef BUFFERS_H
+#define BUFFERS_H
+
+namespace FlyEngine
+{
+	namespace Utils
+	{
+		struct Buffer
+		{
+			unsigned int VAO;
+			unsigned int VBO;
+			unsigned int EBO;
+		};
+	}
+}
+
+#endif // !BUFFERS_H
