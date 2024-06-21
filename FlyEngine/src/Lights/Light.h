7418a562da67323c31a9a88a4263db0f923aeed3
Guido Tello <lpmaleinn@gmail.com>
2024-06-21 14:10:54 -0300

    Create Light Class
    The base class of 3 different types

---
+++ FlyEngine/src/Lights/Light.h
@@ -0,0 +1,17 @@
+#ifndef LIGHT_H
+#define LIGHT_H
+
+#include <Exports/Exports.h>
+
+class FLY_API Light
+{
+private:
+	//Shader* shader;
+public:
+	Light();
+	~Light();
+};
+
+
+
+#endif // !MATERIAL_H
