ef198a5bcd6e66bf4170fdeba6ff91b72d809222
Guido Tello <lpmaleinn@gmail.com>
2024-06-21 14:26:35 -0300

    Update CameraController Class
    Now the cameraController uses the camera just to move it directly

--- FlyEngine/src/CameraController/CameraController.cpp
+++ FlyEngine/src/CameraController/CameraController.cpp
@@ -1,11 +1,13 @@
+#include <string>
+
 #include "CameraController.h"
-#include "Window/Window.h"
+#include "FlyFunctions/Debugger/Debugger.h"
 
 FlyEngine::Utils::Input innerSystemInput;
 
-FlyEngine::CameraController::CameraController(Window* window, KeyCode forward, KeyCode backward, KeyCode up, KeyCode down, KeyCode left, KeyCode right)
+FlyEngine::CameraController::CameraController(Camera* camera, Window* window, KeyCode forward, KeyCode backward, KeyCode up, KeyCode down, KeyCode left, KeyCode right)
 {
-	camera = new Camera(window, FlyEngine::ProjectionType::Perspective, 45.0f, 1920.0f / 1080, 0.0f, 100.0f);
+	this->camera = camera;
 	
 	this->forward = forward;
 	this->backward = backward;
@@ -14,12 +16,18 @@ FlyEngine::CameraController::CameraController(Window* window, KeyCode forward, K
 	this->left = left;
 	this->right = right;
 
+	sensibility = 0.01f;
+
 	innerSystemInput = FlyEngine::Utils::Input(window);
+
+	cameraRotation = CameraRotation();
 }
 
 FlyEngine::CameraController::~CameraController()
 {
-	delete camera;
+	if (camera != nullptr)
+		delete camera;
+	camera = nullptr;
 }
 
 void FlyEngine::CameraController::SetCameraSpecs(ProjectionType projType, float fov, float aspectRatio, float nearPlane, float farPlane)
@@ -35,30 +43,56 @@ void FlyEngine::CameraController::SetCamera(Camera* newCamera)
 	camera = newCamera;
 }
 
+FlyEngine::Camera* FlyEngine::CameraController::GetCamera()
+{
+	return camera;
+}
+
 void FlyEngine::CameraController::Update()
 {
+	bool cameraMoved = false;
+
 	if (innerSystemInput.GetKeyDown(forward))
 	{
-		camera->Translate(0.0f, 0.0f, 0.01f);
+		camera->Translate(0.0f, 0.0f, sensibility);
+		cameraMoved = true;
 	}
 	if (innerSystemInput.GetKeyDown(backward))
 	{
-		camera->Translate(0.0f, 0.0f, -0.01f);
+		camera->Translate(0.0f, 0.0f, -sensibility);
+		cameraMoved = true;
 	}
 	if (innerSystemInput.GetKeyDown(up))
 	{
-		camera->Translate(0.0f, -0.01f, 0.0f);
+		camera->Translate(0.0f, -sensibility, 0.0f);
+		cameraMoved = true;
 	}
 	if (innerSystemInput.GetKeyDown(down))
 	{
-		camera->Translate(0.0f, 0.01f, 0.0f);
+		camera->Translate(0.0f, sensibility, 0.0f);
+		cameraMoved = true;
 	}
 	if (innerSystemInput.GetKeyDown(left))
 	{
-		camera->Translate(-0.01f, 0.0f, 0.0f);
+		camera->Translate(-sensibility, 0.0f, 0.0f);
+		cameraMoved = true;
 	}
 	if (innerSystemInput.GetKeyDown(right))
 	{
-		camera->Translate(0.01f, 0.0f, 0.0f);
+		camera->Translate(sensibility, 0.0f, 0.0f);
+		cameraMoved = true;
+	}
+
+	if (cameraMoved)
+	{
+		std::string text = "- Camera Updated Position: (";
+		text += std::to_string(camera->GetPosition().x);
+		text += ",";
+		text += std::to_string(camera->GetPosition().y);
+		text += ",";
+		text += std::to_string(camera->GetPosition().z);
+		text += ")";
+		Debugger::ConsoleMessage(&text[0], 1, 0, 1, 0);
 	}
 }
+
--- FlyEngine/src/CameraController/CameraController.h
+++ FlyEngine/src/CameraController/CameraController.h
@@ -2,6 +2,7 @@
 #define CAMERACONTROLLER_H
 
 #include "Camera/Camera.h"
+
 #include "Input/Input.h"
 
 namespace FlyEngine
@@ -10,8 +11,8 @@ namespace FlyEngine
 
 	struct FLY_API CameraRotation
 	{
-		float yaw;// = -90.0f; // Yaw inicial
-		float pitch;// = 0.0f; // Pitch inicial
+		float yaw;// = -90.0f; // Yaw inicial rot y
+		float pitch;// = 0.0f; // Pitch inicial rot x
 	};
 
 	class FLY_API CameraController
@@ -24,12 +25,16 @@ namespace FlyEngine
 		KeyCode down;
 		KeyCode left;
 		KeyCode right;
+		CameraRotation cameraRotation;
+		float sensibility;
 	public:
-		CameraController(Window* window, KeyCode forward, KeyCode backward, KeyCode up, KeyCode down, KeyCode left, KeyCode right);
+		CameraController(Camera* camera, Window* window, KeyCode forward, KeyCode backward, KeyCode up, KeyCode down, KeyCode left, KeyCode right);
 		~CameraController();
 		
 		void SetCameraSpecs(ProjectionType projType, float fov, float aspectRatio, float nearPlane, float farPlane);
 		void SetCamera(Camera* newCamera);
+
+		Camera* GetCamera();
 
 		void Update();
 	};
