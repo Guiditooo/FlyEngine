7e0abb5415bc2feb07992fc944de0712422bf810
Guido Tello <lpmaleinn@gmail.com>
2024-06-26 13:20:18 -0300

    Recode Cube Vertex & Index Arrays
    In order to get better vertex atributes control.
    Added Normal and UV, for every face.

--- FlyEngine/src/Cube/Cube.cpp
+++ FlyEngine/src/Cube/Cube.cpp
@@ -14,62 +14,116 @@ namespace FlyEngine
 
 		Cube::Cube() : Entity3D("Cube")
 		{
+			const unsigned int floatSize = sizeof(float);
+			const unsigned int vertexStride = (3 + 3 + 2 + 3) * floatSize;
+			// 3 para posición, 3 para color, 2 para UV, 3 para normal
+			
+			VertexAttribute va;
+			
 			material = new Material();
 
-			vertex =
+			vertex = 
 			{
-				-0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, //0 ARRIBA IZQ FRENTE
-				 0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, //1 ARRIBA DER FRENTE
-				-0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f, //2 ABAJO  IZQ FRENTE
-				 0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f, //3 ABAJO  DER FRENTE
+				// Position          Color             UV	        Normal
+				// Front
+				-0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f,  0.0f,  0.0f,  1.0f, // 0
+				 0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,  0.0f,  0.0f,  1.0f, // 1
+				-0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,  0.0f,  0.0f,  1.0f, // 2
+				 0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,  0.0f,  0.0f,  1.0f, // 3
 
-				-0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f, //4 ARRIBA IZQ DETRAS
-				 0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f, //5 ARRIBA DER DETRAS
-				-0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, //6 ABAJO  IZQ DETRAS
-				 0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f  //7 ABAJO  DER DETRAS
+				// Back
+			    -0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f,  0.0f,  0.0f, -1.0f, // 4
+				 0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,  0.0f,  0.0f, -1.0f, // 5
+				-0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,  0.0f,  0.0f, -1.0f, // 6
+				 0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,  0.0f,  0.0f, -1.0f, // 7
+
+				// Left
+				-0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, -1.0f,  0.0f,  0.0f, // 8
+				-0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f, -1.0f,  0.0f,  0.0f, // 9
+				-0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f, -1.0f,  0.0f,  0.0f, // 10
+				-0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f, -1.0f,  0.0f,  0.0f, // 11
+
+				// Right
+				 0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,  1.0f,  0.0f,  0.0f, // 12
+				 0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,  1.0f,  0.0f,  0.0f, // 13
+				 0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f,  1.0f,  0.0f,  0.0f, // 14
+				 0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,  1.0f,  0.0f,  0.0f, // 15
+
+				// Top
+				-0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f,  0.0f,  1.0f,  0.0f, // 16
+			 	 0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,  0.0f,  1.0f,  0.0f, // 17
+				-0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f,  0.0f,  1.0f,  0.0f, // 18
+				 0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,  0.0f,  1.0f,  0.0f, // 19
+
+				// Bot
+				-0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,  0.0f, -1.0f,  0.0f, // 20
+				 0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,  0.0f, -1.0f,  0.0f, // 21
+				-0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,  0.0f, -1.0f,  0.0f, // 22
+				 0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,  0.0f, -1.0f,  0.0f, // 23
 			};
 
-			index =
+			index = 
 			{
-				// Cara frontal
+				// Front
 				0, 1, 2,
 				1, 3, 2,
 
-				// Cara trasera
+				// Back
 				4, 6, 5,
 				5, 6, 7,
 
-				// Cara izquierda
-				0, 2, 4,
-				4, 2, 6,
+				// Left
+				8, 9, 10,
+				10, 9, 11,
 
-				// Cara derecha
-				1, 5, 3,
-				5, 7, 3,
+				// Right
+				12, 14, 13,
+				13, 14, 15,
 
-				// Cara superior
-				0, 4, 1,
-				1, 4, 5,
+				// Top
+				16, 18, 17,
+				17, 18, 19,
 
-				// Cara inferior
-				2, 3, 6,
-				3, 7, 6
-			};
+				// Bot
+				20, 21, 22,
+				21, 23, 22,
+			};			
 
 			indexCount = index.size();
 			vertexCount = 8;
 			vertexSize = 6;
 
-			for (short i = 0; i < vertexCount; i++)
-			{
-				VertexAttribute va;
-				va.elementSize = 3;
-				va.variableType = GL_FLOAT;
-				va.isNormalized = GL_FALSE;
-				va.sizeOfVertex = sizeof(float) * vertexSize;
-				va.offset = i * sizeof(float) * 3;
-				vertexAttributes.push_back(va);
-			}
+			//Posicion
+			va.elementSize = 3;
+			va.variableType = GL_FLOAT;
+			va.isNormalized = GL_FALSE;
+			va.sizeOfVertex = vertexStride;
+			va.offset = 0;
+			vertexAttributes.push_back(va);
+			
+			//Color
+			va.elementSize = 3;
+			va.variableType = GL_FLOAT;
+			va.isNormalized = GL_FALSE;
+			va.sizeOfVertex = vertexStride;
+			va.offset = 3 * floatSize; //Después de la posición)
+			vertexAttributes.push_back(va);
+
+			//UV
+			va.elementSize = 2;
+			va.variableType = GL_FLOAT;
+			va.isNormalized = GL_FALSE;
+			va.sizeOfVertex = vertexStride;
+			va.offset = (3 + 3) * floatSize; //Después del color)
+			vertexAttributes.push_back(va);
+
+			//Normal
+			va.elementSize = 3;
+			va.variableType = GL_FLOAT;
+			va.isNormalized = GL_FALSE;
+			va.sizeOfVertex = vertexStride;
+			va.offset = (3 + 3 + 2) * floatSize; //Después del UV)
+			vertexAttributes.push_back(va);
 
 			//Renderer::SetVertexAttributes(vertexAttributes, vertexCount);
 
