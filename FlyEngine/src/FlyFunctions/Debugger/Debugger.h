81b3e6b5bb98a677b527059d6723fa1b189d1939
Guido Tello <lpmaleinn@gmail.com>
2024-06-26 13:23:16 -0300

    Set Default Values to Debugger Functions

--- FlyEngine/src/FlyFunctions/Debugger/Debugger.h
+++ FlyEngine/src/FlyFunctions/Debugger/Debugger.h
@@ -21,10 +21,10 @@ namespace FlyEngine
 
 			static void ConsoleMessage(const char* text);
 			static void ConsoleMessage(const char* initialMsg, glm::vec3 vector3);
-			static void ConsoleMessage(const char* text, short beforeSpacesCount, short afterSpacesCount = 0, short beforeEndlCount = 0, short afterEndlCount = 0);
-			static void ConsoleMessage(const char* initialMsg, glm::vec3 vector3, short beforeSpacesCount = 0, short afterSpacesCount = 0, short beforeEndlCount = 0, short afterEndlCount = 0);
-			static void ConsoleMessageID(const char* text, short beforeSpacesCount = 0, short afterSpacesCount = 0, short beforeEndlCount = 0, short afterEndlCount = 0);
-			static void ConsoleMessageID(const char* initialMsg, glm::vec3 vector3, short beforeSpacesCount = 0, short afterSpacesCount = 0, short beforeEndlCount = 0, short afterEndlCount = 0);
+			static void ConsoleMessage(const char* text, short beforeSpacesCount, short afterSpacesCount = 1, short beforeEndlCount = 0, short afterEndlCount = 1);
+			static void ConsoleMessage(const char* initialMsg, glm::vec3 vector3, short beforeSpacesCount = 0, short afterSpacesCount = 1, short beforeEndlCount = 0, short afterEndlCount = 1);
+			static void ConsoleMessageID(const char* text, short beforeSpacesCount = 0, short afterSpacesCount = 1, short beforeEndlCount = 0, short afterEndlCount = 1);
+			static void ConsoleMessageID(const char* initialMsg, glm::vec3 vector3, short beforeSpacesCount = 0, short afterSpacesCount = 1, short beforeEndlCount = 0, short afterEndlCount = 1);
 
 		};
 
