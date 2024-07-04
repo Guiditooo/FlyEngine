0dc9b3fd5efad93895e3e082d7b88edef3288d17
Guido Tello <lpmaleinn@gmail.com>
2024-07-03 22:19:44 -0300

    Create MaterialSpecification
    In order to have every Material from the Material List with their specs, i created this. This class is so usefull when i have to create the uniforms.

---
+++ FlyEngine/src/MaterialSpecification/MaterialSpecification.h
@@ -0,0 +1,37 @@
+#ifndef MATERIAL_SPEC_H
+#define MATERIAL_SPEC_H
+
+#include <Exports/Exports.h>
+#include <glm/glm.hpp>
+
+#include "Material/MaterialList.h"
+
+namespace FlyEngine
+{
+    namespace Materials
+    {
+        class FLY_API MaterialSpecification
+        {
+        private:
+            glm::vec3 ambient;
+            glm::vec3 diffuse;
+            glm::vec3 specular;
+            float shininess;
+        public:
+            MaterialSpecification(glm::vec3 amb, glm::vec3 diff, glm::vec3 spec, float shin)
+                : ambient(amb), diffuse(diff), specular(spec), shininess(shin) {}
+            MaterialSpecification();
+            ~MaterialSpecification();
+            void SetSpecs(MaterialList material);
+
+            glm::vec3 GetAmbient();
+            glm::vec3 GetDiffuse();
+            glm::vec3 GetSpecular();
+            float GetShininess();
+        };
+
+        
+    }
+}
+
+#endif // !MATERIAL_SPEC_H
