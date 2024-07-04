3d69cc497a72a987f3cb5414f6e4d80ffd6f6cee
Guido Tello <lpmaleinn@gmail.com>
2024-07-03 22:18:37 -0300

    Create MaterialList
    In order to have some materials i have got this list

---
+++ FlyEngine/src/Material/MaterialList.h
@@ -0,0 +1,41 @@
+#ifndef MATERIAL_LIST_H
+#define MATERIAL_LIST_H
+
+#include "Exports/Exports.h"
+
+namespace FlyEngine
+{
+	namespace Materials
+	{
+		enum class FLY_API MaterialList
+		{
+            Emerald,
+            Jade,
+            Obsidian,
+            Pearl,
+            Ruby,
+            Turquoise,
+            Brass,
+            Bronze,
+            Chrome,
+            Copper,
+            Gold,
+            Silver,
+            BlackPlastic,
+            CyanPlastic,
+            GreenPlastic,
+            RedPlastic,
+            WhitePlastic,
+            YellowPlastic,
+            BlackRubber,
+            CyanRubber,
+            GreenRubber,
+            RedRubber,
+            WhiteRubber,
+            YellowRubber,
+            None
+		};
+	}
+}
+
+#endif // !MATERIAL_H
