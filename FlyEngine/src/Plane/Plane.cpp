344bdd43889bdbad41ece7807795566ebda01810
Guido Tello <lpmaleinn@gmail.com>
2024-11-14 08:05:48 -0300

    Update Plane Class
    Added variable, setters & getters for the planeID.
    Added getter for the normal.

--- FlyEngine/src/Plane/Plane.cpp
+++ FlyEngine/src/Plane/Plane.cpp
@@ -2,29 +2,55 @@
 
 namespace FlyEngine
 {
+	int Plane::planeCount = 0;
+
+
+	void Plane::AsignID()
+	{
+		ID = planeCount;
+		planeCount++;
+	}
 
 	Plane::Plane()
 	{
 		normal = glm::vec3(0.0f, 0.0f, 0.0f);
-		distance = 0.0f;
+		distance = 0.0f; 
+		AsignID();
 	}
 
 	Plane::Plane(glm::vec3 normal, float distance)
 	{
 		this->normal = glm::normalize(normal);
 		this->distance = distance;
+		AsignID();
 	}
 
 	Plane::Plane(glm::vec3 normal, glm::vec3 point)
 	{
 		this->normal = glm::normalize(normal);
 		this->distance = glm::dot(normal, point);
+		AsignID();
 	}
 
 	Plane::Plane(glm::vec3 p1, glm::vec3 p2, glm::vec3 p3)
 	{
 		normal = glm::normalize(glm::cross(p2 - p1, p3 - p1));
 		distance = glm::dot(normal, p1);
+		AsignID();
+	}
+
+	Plane::~Plane()
+	{
+	}
+
+	int Plane::GetID()
+	{
+		return ID;
+	}
+
+	glm::vec3 Plane::GetNormal()
+	{
+		return normal;
 	}
 
 	float Plane::GetDistanceToPoint(glm::vec3 point)
--- FlyEngine/src/Plane/Plane.h
+++ FlyEngine/src/Plane/Plane.h
@@ -10,8 +10,13 @@ namespace FlyEngine
 	class FLY_API Plane
 	{
 	private:
+		static int planeCount;
+
+		int ID;
 		glm::vec3 normal; // Vector normal al Plane
 		float distance;  // Distancia desde el origen
+
+		void AsignID();
 	public:
 
 		Plane();
@@ -19,7 +24,12 @@ namespace FlyEngine
 		Plane(glm::vec3 normal, glm::vec3 point);
 		Plane(glm::vec3 p1, glm::vec3 p2, glm::vec3 p3);
 
+		~Plane();
+
+		int GetID();
+		glm::vec3 GetNormal();
 		float GetDistanceToPoint(glm::vec3 point);
+
 		bool IsPointInFront(glm::vec3 point);
 	};
 
