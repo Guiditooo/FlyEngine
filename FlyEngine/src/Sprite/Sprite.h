ca85af44132031d4805d9b512457ec4256ed3249
Guido Tello <lpmaleinn@gmail.com>
2025-04-02 00:01:41 -0300

    Create Sprite Class

--- FlyEngine/src/Sprite/Sprite.h
+++ FlyEngine/src/Sprite/Sprite.h
@@ -1,1 +1,36 @@
-#pragma once
+#ifndef SPRITE_H
+#define SPRITE_H
+
+#include <Entity2D/Entity2D.h>
+
+namespace FlyEngine
+{
+	namespace Entities
+	{
+		struct SpriteDims
+		{
+			int width;
+			int height;
+		};
+
+		class FLY_API Sprite : public Entity2D
+		{
+			
+
+		private:
+			static int internalID;
+			std::string textureCode;
+			int texture;
+			int width;
+			int height;
+		public:
+			Sprite(const char* path, bool creationMessage = false);
+			SpriteDims GetDimentions();
+			int GetTexture();
+			~Sprite();
+		};
+
+	}
+
+}
+#endif // !SPRITE_H
